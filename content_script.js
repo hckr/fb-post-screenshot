b43a5cc09479e4e931ac85798b63d00467b25fd5
Luke <luke@luke.co.pl>
2017-06-29 21:50:50 +0200

    Fix click handler detaching and reattaching

--- content_script.js
+++ content_script.js
@@ -73,12 +73,13 @@ function callback(mutations) {
                     let save_screenshot_anon = createMenuElement('Save anonymized screenshot', 'Creating screenshot...');
                     save_screenshot_anon.onclick = clickHandler.bind(save_screenshot_anon, true);
                     function clickHandler(anonymize) {
+                        let old_onclick = this.onclick;
                         this.onclick = undefined;
                         this.toggleText();
                         let post_window = window.open(permalink, 's', 'width=300, height=100, left=0, top=0, resizable=yes, toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no');
                         post.click();
                         this.toggleText();
-                        this.onclick = clickHandler;
+                        this.onclick = old_onclick;
                         function responseCallback(response) {
                             let post_id = permalink.match(/(\d{2,})(?!.*\d{2,})/)[1],
                                 part_nr = 1;
