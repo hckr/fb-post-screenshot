548a0c197bd3505f7bed55e6acf1e2eb06428c3e
Luke <luke@luke.co.pl>
2019-03-16 22:47:47 +0100

    Add a browser_action with a popup (resolves #10)

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById('version').innerText = `v. ${browser.runtime.getManifest().version}`;
+
+document.getElementById('show-options').addEventListener('click', _ => {
+    browser.runtime.openOptionsPage();
+    window.close();
+});
+
+document.body.addEventListener('click', e => e.target.closest('a') && setTimeout(window.close, 50));
